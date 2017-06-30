cbb11bf94c2b3a73ac4a86aaa258488f19dfcc7e
drzl777 <andrew.zheng725@gmail.com>
2017-06-30 12:14:03 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,35 @@
 // Code SimpleComponentHere Here
+
+import React from 'react'
+
+class SimpleComponent extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
+  toggleMood() {
+    if (this.state.mood === 'happy') {
+      return 'sad'
+    } else {
+      return 'happy'
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      mood: this.toggleMood()
+    })
+  }
+
+
+  render() {
+    return (
+      <div onClick={this.handleClick.bind(this)}>{this.state.mood}</div>
+    )
+  }
+}
+
+export default SimpleComponent
